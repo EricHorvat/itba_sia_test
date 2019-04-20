df8b9bf5c38b5d4291587b6e2ff204d33b621e07
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-04-19 21:24:46 -0300

    [FIX] how..test is more simple

--- how_will_test.sh
+++ how_will_test.sh
@@ -1,7 +1,6 @@
-# REPOSITORY_NAME = sia-2019-1c-X
-mkdir $REPOSITORY_NAME
+# `pwd` = ~/itba_sia_test
+# `ls -d */` = src/ $REPOSITORY_NAME/
 mkdir output
-cd $REPOSITORY_NAME && mvn package | tee ../output/student_package_result.txt
+cd $REPOSITORY_NAME && mvn install | tee ../output/student_installing_result.txt
 cd ..
-mvn install:install-file -Dfile=./$REPOSITORY_NAME/target/gps-1.0.jar | tee output/student_package_installing_result.txt
 mvn package | tee output/test_result.txt
